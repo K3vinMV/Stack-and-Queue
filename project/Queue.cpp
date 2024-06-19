00562df8eda0f6707a81b4a7f4cc516052eb3b93
Kevin Melendez <kevinmelendez3535@gmail.com>
2024-06-19 00:02:00 -0600

    Queue files updated

--- project/Queue.cpp
+++ project/Queue.cpp
@@ -1,6 +1,132 @@
 #include "Queue.h"
+#include "Student.h"
 
 Queue::Queue()
 {
     //ctor
+}
+
+Queue::Queue(NodeQueue *ptr, NodeQueue *ptr1)
+{
+    h=ptr;
+    t=ptr1;
+}
+
+void Queue::initialize()
+{
+    h=nullptr;
+    t=nullptr;
+}
+
+void Queue::enqueue(class Student data)
+{
+    NodeQueue *tmp=new NodeQueue(data,nullptr,nullptr);
+    tmp->data=data;
+
+    if(!h)
+    {
+        h=tmp;
+        t=tmp;
+    }else if(t)
+    {
+        t->next=tmp;
+        tmp->prev=t;
+        t=tmp;
+    }
+}
+
+void Queue::Dequeue()
+{
+    NodeQueue *aux=h;
+
+    if(t)
+    {
+        h=h->next;
+        h->prev=nullptr;
+        delete aux;
+    }else
+    {
+        cout<<"The queue is empty"<<endl;
+    }
+}
+
+NodeQueue* Queue::first()
+{
+    if(h)
+    {
+      return h;
+    }else
+    {
+        return nullptr;
+    }
+}
+
+NodeQueue* Queue::last()
+{
+    if(h)
+    {
+      return t;
+    }else
+    {
+        return nullptr;
+    }
+}
+
+void Queue::isEmpty()
+{
+    if (h==nullptr)
+    {
+        cout<<"Queue is empty"<<endl;
+    }else
+    {
+        cout<<"Queue has elements"<<endl;
+    }
+}
+
+int Queue::Size()
+{
+    int i=0;
+    NodeQueue *aux=h;
+
+    if(h==nullptr)
+    {
+        return i;
+    }else
+    {
+        while(aux)
+        {
+            aux=aux->next;
+            i++;
+        }
+        return i;
+    }
+}
+
+void Queue::removeAll()
+{
+    while(h)
+    {
+        NodeQueue *aux=h;
+        h=aux->next;
+        delete aux;
+    }
+}
+
+void Queue::display()
+{
+    NodeQueue *aux=t;
+    if(h)
+    {
+        while(aux)
+        {
+            cout<<"Name: "<<aux->data.name<<endl;
+            cout<<"Age: "<<aux->data.age<<endl;
+            cout<<"Code: "<<aux->data.code<<endl;
+            cout<<"\n"<<endl;
+            aux=aux->prev;
+        }
+    }else
+    {
+        cout<<"Queue is empty"<<endl;
+    }
 }
--- project/Queue.h
+++ project/Queue.h
@@ -1,15 +1,31 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include "NodeQueue.h"
+#include "Student.h"
+#include <iostream>
 
+using namespace std;
 
 class Queue
 {
     public:
+        NodeQueue *h;
+        NodeQueue *t;
+
         Queue();
+        Queue(NodeQueue*,NodeQueue*);
 
-    protected:
+        void initialize();
+        void enqueue(class Student);
+        void Dequeue();
+        NodeQueue *first();
+        NodeQueue *last();
+        void isEmpty();
+        int Size();
+        void removeAll();
+        void display();
 
-    private:
+
 };
 
 #endif // QUEUE_H
