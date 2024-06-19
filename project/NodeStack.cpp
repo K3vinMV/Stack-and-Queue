a63da229924b5494f43a49cedcb2e2cc923a3cf2
Kevin Melendez <kevinmelendez3535@gmail.com>
2024-06-18 23:37:26 -0600

    NodeStack files added

--- project/NodeStack.cpp
+++ project/NodeStack.cpp
@@ -1,6 +1,17 @@
 #include "NodeStack.h"
+#include "Student.h"
 
 NodeStack::NodeStack()
 {
+    Student data;
+    next=nullptr;
+    prev=nullptr;
     //ctor
+}
+
+NodeStack::NodeStack(class Student data, NodeStack *next, NodeStack *prev)
+{
+    this->data=data;
+    this->next=next;
+    this->prev=prev;
 }
--- project/NodeStack.h
+++ project/NodeStack.h
@@ -1,15 +1,18 @@
 #ifndef NODESTACK_H
 #define NODESTACK_H
-
+#include "Student.h"
 
 class NodeStack
 {
     public:
+        NodeStack *next;
+        NodeStack *prev;
+
+        Student data;
         NodeStack();
+        NodeStack(class Student, NodeStack*, NodeStack*);
 
-    protected:
-
-    private:
 };
+
 
 #endif // NODESTACK_H
