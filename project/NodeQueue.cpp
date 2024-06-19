cfaeb96ebd970431fbafc72e6a86bc01cfedb636
Kevin Melendez <kevinmelendez3535@gmail.com>
2024-06-18 23:30:06 -0600

    NodeQueue files updated

--- project/NodeQueue.cpp
+++ project/NodeQueue.cpp
@@ -1,6 +1,14 @@
 #include "NodeQueue.h"
+#include "Student.h"
 
 NodeQueue::NodeQueue()
 {
+    Student data;
+    next=nullptr;
     //ctor
+}
+NodeQueue::NodeQueue(class Student data, NodeQueue *next)
+{
+    this->data=data;
+    this->next=next;
 }
--- project/NodeQueue.h
+++ project/NodeQueue.h
@@ -1,15 +1,18 @@
 #ifndef NODEQUEUE_H
 #define NODEQUEUE_H
+#include "Student.h"
 
 
 class NodeQueue
 {
     public:
+        NodeQueue *next;
+
+        Student data;
         NodeQueue();
+        NodeQueue(class Student, NodeQueue*);
 
-    protected:
-
-    private:
 };
+
 
 #endif // NODEQUEUE_H
